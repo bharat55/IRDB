5dfbe00ddb95364a6d50694861c46b9201a190a2
Bharat Rathor <bharat.rathor@gemsessence.com>
2023-08-27 09:28:55 +0530

    Adding react router dom

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Books from "./Books";
+import Book from "./Book";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div> Hello world</div>;
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/books/:id" element={<Book />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   namespace :admin do
-      resources :books
-      resources :reviews
-      resources :stores
-      resources :users
+    resources :books
+    resources :reviews
+    resources :stores
+    resources :users
 
-      root to: "books#index"
-    end
+    root to: "books#index"
+  end
+
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
+  
   post "/graphql", to: "graphql#execute"
   devise_for :users
   root 'home#index'
