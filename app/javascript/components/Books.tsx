c24b82aa7fdda3d9ae51fd5b9c7471000cc7cbb8
Bharat Rathor <bharat.rathor@gemsessence.com>
2023-08-28 12:55:33 +0530

    Setup appolo graphql

--- app/javascript/components/Books.tsx
+++ app/javascript/components/Books.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_BOOKS = gql`
+  query GetBooks {
+    books {
+      id
+      name
+      authorName
+      publishedAt
+      tagLine
+      category
+      summary
+    }
+  }
+`;
 
 function Books() {
-  return <div>Books</div>;
+  const { loading, error, data } = useQuery(GET_BOOKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.books.map(
+    ({ id, name, authorName, publishedAt, tagLine, category, summary }) => (
+      <div key={id}>
+        <h3>{name}</h3>
+        <div>{authorName}</div>
+        <div>{publishedAt}</div>
+        <div>{tagLine}</div>
+        <div>{category}</div>
+        <div>{summary}</div>
+      </div>
+    )
+  );
 }
 
 export default Books;
--- app/javascript/components/root.tsx
+++ app/javascript/components/root.tsx
@@ -1,7 +1,43 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const csrfToken = document
+    .querySelector('meta[name="csrf-token"]')
+    .getAttribute("content");
+  return {
+    headers: {
+      ...headers,
+      "X-CSRF-Token": csrfToken,
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("root");
+  const root = createRoot(container); // createRoot(container!) if you use TypeScript
+  root.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+});
